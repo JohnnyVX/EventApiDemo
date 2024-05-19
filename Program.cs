8e8fc9227f50b3a13f31cd9f7ff3f2a8a0d1ee7c
johnnyvreply <johnnyvreply@gmail.com>
2024-05-19 18:50:30 -0400

    built-in exception handler middleware

--- Program.cs
+++ Program.cs
@@ -1,42 +1,20 @@
-using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models; // Add this line
-using Service.Events;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllers();
-builder.Services.AddScoped<EventService>();
-
-// Add Swagger services
-builder.Services.AddSwaggerGen(c =>
+namespace EventApiDemo
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "EventApiDemo", Version = "v1" });
-});
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
 }
-
-app.UseHttpsRedirection();
-
-// Use Swagger middleware
-app.UseSwagger();
-app.UseSwaggerUI(c =>
-{
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "EventApiDemo v1");
-});
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
---
+++ Startup.cs
@@ -0,0 +1,92 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EventApiDemo.Filters;
+using EventApiDemo.Middleware;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.OpenApi.Models;
+
+namespace EventApiDemo
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers(options =>
+            {
+                // Uncomment the following line to use the custom exception filter
+                // options.Filters.Add<CustomExceptionFilter>();
+            });
+
+            // Register the Swagger generator, defining one or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
+
+            // Register the EventService as a scoped service
+            services.AddScoped<Service.Events.EventService>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
+        {
+            // if (env.IsDevelopment())
+            // {
+            //     // app.UseDeveloperExceptionPage();
+            // }
+            // else
+            // {
+
+            // }
+
+            // Uncomment the following block to use the built-in exception handler middleware
+            // /*
+            app.UseExceptionHandler(appBuilder =>
+            {
+                appBuilder.Run(async context =>
+                {
+                    context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                    context.Response.ContentType = "application/json";
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature != null)
+                    {
+                        logger.LogError(contextFeature.Error, "UseExceptionHandlering: An error occurred while processing your request.");
+                        await context.Response.WriteAsync(new
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            Message = "UseExceptionHandlering: An error occurred while processing your request. Please try again later."
+                        }.ToString());
+                    }
+                });
+            });
+            // */
+
+            // Uncomment the following line to use the custom exception middleware
+            // app.UseMiddleware<ExceptionMiddleware>();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
